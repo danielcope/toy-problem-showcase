278ef5592d46545d9d7da123b0a8fbfcb9deec6d
Daniel Cope <danielcope@users.noreply.github.com>
2021-03-02 22:03:03 -0800

    finished all but SUM

---
+++ src/components/Topics/Sum.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+
+class Sum extends Component{
+
+    render(){
+        return (
+        <p> SumComponent</p>
+        )
+    }
+}
+
+export default Sum
